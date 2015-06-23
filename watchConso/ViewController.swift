996593044b4b12ccae6b761c4a3b5ae810ac5bb1
JeToO <JeToO@MacBook-Air-de-JeToO.local>
2015-06-23 11:55:00 +0200

    Ecran iOS v0.1 - DSI

--- watchConso/ViewController.swift
+++ watchConso/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    private let dwarves = [
+        "EDF", "GDF", "Virement Papa", "Carrefour",
+        "SFR", "Amazon"
+    ]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell = tableView.dequeueReusableCellWithIdentifier(
+                simpleTableIdentifier) as? UITableViewCell
+            if (cell == nil) {
+                cell = UITableViewCell(
+                    style: UITableViewCellStyle.Value1,
+                    reuseIdentifier: simpleTableIdentifier)
+            }
+            let image = UIImage(named: "logo-vac")
+            cell!.imageView!.image = image
+        
+            cell!.detailTextLabel?.text = "125,75 euros"
 
+            cell!.textLabel!.text = dwarves[indexPath.row]
+            
+            
+            return cell!
+    }
+
+    
 }
 
